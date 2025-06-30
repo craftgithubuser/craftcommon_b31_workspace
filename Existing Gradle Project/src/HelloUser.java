4bc272067d1b0c306069a933a8343d7f61684f72
zemen2024 <zemenc@gmail.com>
2025-06-29 21:57:40 -0700

    make change

---
+++ Existing Gradle Project/src/HelloUser.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class HelloUser {;
+    private static final String Zemen = null;
+
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello, " + Zemen+ "! Welcome to Java.");
+        
+        scanner.close();
+    }
+}
--- Existing Gradle Project/src/zemen.java
+++ Existing Gradle Project/src/zemen.java
@@ -3,7 +3,7 @@ public class zemen {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+ 
 	}
 
 }
